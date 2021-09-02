fdb6c6f95179d26acb0bf91f44a0ca6a095639a0
Jacob <jsilver@tempo.io>
2021-09-02 15:56:00 -0400

    tempo

--- src/email.js
+++ src/email.js
@@ -1,67 +1,96 @@
 import React from 'react';
-import { readFileSync } from 'fs';
+import {readFileSync} from 'fs';
 
 import {
-  Mjml,
-  MjmlHead,
-  MjmlTitle,
-  MjmlPreview,
-  MjmlBody,
-  MjmlSection,
-  MjmlColumn,
-  MjmlButton,
-  MjmlImage,
-  MjmlStyle,
-  MjmlText
+    Mjml,
+    MjmlHead,
+    MjmlTitle,
+    MjmlPreview,
+    MjmlBody,
+    MjmlSection,
+    MjmlColumn,
+    MjmlButton,
+    MjmlImage,
+    MjmlStyle,
+    MjmlText,
+    MjmlWrapper,
 } from 'mjml-react';
 
 const css = readFileSync('./assets/styles.css').toString();
 
 export const generate = () => {
-  return (
-    <Mjml>
-      <MjmlHead>
-        <MjmlTitle>Last Minute Offer</MjmlTitle>
-        <MjmlPreview>Last Minute Offer...</MjmlPreview>
-        <MjmlStyle>{css}</MjmlStyle>
-        <MjmlStyle>{`
-          .blue-column {
-            background-color: blue;
-          }
-        `}</MjmlStyle>
-        <MjmlStyle inline>{`
-          .red-column {
-            background-color: red;
-          }
-        `}</MjmlStyle>
-      </MjmlHead>
-      <MjmlBody width={500}>
-        <MjmlSection fullWidth backgroundColor="#efefef">
-          <MjmlColumn>
-            <MjmlImage src="https://static.wixstatic.com/media/5cb24728abef45dabebe7edc1d97ddd2.jpg" />
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn>
-            <MjmlButton padding="20px" backgroundColor="#346DB7" href="https://www.wix.com/">
-              I like it!
-            </MjmlButton>
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn cssClass="blue-column">
-            <MjmlText>I am blue</MjmlText>
-          </MjmlColumn>
-          <MjmlColumn cssClass="red-column">
-            <MjmlText>I am red</MjmlText>
-          </MjmlColumn>
-        </MjmlSection>
-        <MjmlSection>
-          <MjmlColumn>
-            <MjmlText><a href="/2">Open Second Template</a></MjmlText>
-          </MjmlColumn>
-        </MjmlSection>
-      </MjmlBody>
-    </Mjml>
-  );
+    const userEmail = '{{ user.email }}';
+    const url = '{{ url }}';
+    return (
+        <Mjml>
+            <MjmlHead>
+                <MjmlTitle>Please verify your email</MjmlTitle>
+                <MjmlPreview>Please verify your email...</MjmlPreview>
+                <MjmlStyle>{css}</MjmlStyle>
+            </MjmlHead>
+            <MjmlBody backgroundColor="#f5f5f5">
+                <MjmlWrapper borderRadius="5px" padding="0" backgroundColor="white">
+                    <MjmlSection padding="5px" borderRadius="5px 5px 0 0" backgroundColor="#003961">
+                        <MjmlColumn>
+                            <MjmlImage
+                                width="100px"
+                                src="https://static.tempo.io/one-frontend/static/images/TempoLogoLight.png"
+                            />
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlImage
+                                width="217px"
+                                src="https://static.tempo.io/one-frontend/static/images/Welcome.png"
+                            />
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlText align="center">
+                                <strong>Verify your email to finish signing up</strong>
+                            </MjmlText>
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlText padding="12px 48px" align="center">
+                                Thank you for choosing Tempo One. Please confirm that{' '}
+                                <strong>{userEmail}</strong> is your e-mail address by clicking on
+                                the verify button below within 48 hours.
+                            </MjmlText>
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlButton
+                                backgroundColor="#003961"
+                                href={url}
+                                borderRadius="24px"
+                                width="141px"
+                            >
+                                Confirm
+                            </MjmlButton>
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlText fontSize="12px" align="center">
+                                Need Help?
+                            </MjmlText>
+                        </MjmlColumn>
+                    </MjmlSection>
+                    <MjmlSection padding="0">
+                        <MjmlColumn>
+                            <MjmlText align="center">
+                                <a href="https://www.tempo.io/documentation">Help Center</a> |{' '}
+                                <a href="mailto: support@tempo.io">Support@tempo.io</a>
+                            </MjmlText>
+                        </MjmlColumn>
+                    </MjmlSection>
+                </MjmlWrapper>
+            </MjmlBody>
+        </Mjml>
+    );
 };
